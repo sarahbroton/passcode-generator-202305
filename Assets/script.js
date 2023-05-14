a26ea12280cc13ef247b2342caa64ecd0b58c3bb
sarahbroton <sarah.broton@gmail.com>
2023-05-14 11:51:29 -0500

    ihavenotideawhatiamdoing-update-05

--- Assets/script.js
+++ Assets/script.js
@@ -11,7 +11,16 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+    const input = prompt(); 
+    alert(input); 
+
+    if(confirm("How many characters would you like your password to contain?"))
+      console.log('ok'); 
+
+// function genPassword() {
+//   var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+//   var passwordLength = ""
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
