09347ddc9f13ea02f6a552d7521b198713bc7b4e
sarahbroton <sarah.broton@gmail.com>
2023-05-17 16:03:29 -0500

    popupwindowfunbygeorgeithinkigotthispart-update-08

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,7 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+generateBtn.onclick = popupInfo;
 
 // Write password to the #password input
 function writePassword() {
@@ -13,79 +14,38 @@ function writePassword() {
 }
 
 function popupInfo() {
+  while(true){
+    var passwordLength = parseInt(prompt("Choose between 8 and 128 characters in length."), 10);
+    if (passwordLength < 8 || passwordLength > 129) {
+      alert("Invalid password length. Password needs to be greater than 8 and less than 129");
+    }
+    else{
+      break
+    }
+  }
+  console.log("passwordLength", passwordLength); 
   var specialCharacters = confirm("Do you want special characters?");
-  var passwordLength = parseInt (prompt ("Choose between 8 and 128 characters in length."), 10)
-    // console.log(passwordLength); 
-    // console.log(typeof passwordLength); 
-    var passwordChoices = {
-      length: passwordLength, 
-      specialCharacters: specialCharacters, 
+  console.log("specialCharacter", specialCharacters)
+  var numericCharacters = confirm("Do you want numeric characters?");
+  console.log("numericCharacter", numericCharacters)
+  var lowercaseCharacters = confirm("Do you want lowercase characters?");
+  console.log("lowercaseCharacter", lowercaseCharacters)
+  var uppercaseCharacters = confirm("Do you want uppercase characters?");
+  console.log("uppercaseCharacter", uppercaseCharacters)
+  var passwordChoices = {
+    length: passwordLength,
+    specialCharacters: specialCharacters,
+    numericCharacters: numericCharacters,
+    lowercaseCharacters: lowercaseCharacters,
+    uppercaseCharcters: uppercaseCharacters,
 
+  };
 
-    }; 
-    return passwordChoices
+  console.log(passwordChoices);
+  return passwordChoices
 
 }
 
-
-    // console.log(popupInfo()); 
-
-  
-
-
-// global
-//   var numbers [array]; 
-//   var uppcase [array]; 
-//   var lowercase [array]; 
-
-
-//   concat (arrays + arrays)
-//   print array as string
-
-// *ask # of characters, first
-
-
-  // var popup = {
-  //   chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-  //   passwordLength = ; 
-  //   .toUppercase; 
-  // }
-
-  //   console.log(popup); 
-
-// if
-
-
-    // const input = prompt(); 
-    // alert(input); 
-
-    // if(confirm("How many characters would you like your password to contain?"))
-    //   console.log('ok'); 
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-// var password=document.getElementById("password");
-
-//  function genPassword() {
-//     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     var passwordLength = 12;
-//     var password = "";
-//  for (var i = 0; i <= passwordLength; i++) {
-//    var randomNumber = Math.floor(Math.random() * chars.length);
-//    password += chars.substring(randomNumber, randomNumber +1);
-//   }
-//         document.getElementById("password").value = password;
-//  }
-
-// function copyPassword() {
-//   var copyText = document.getElementById("password");
-//   copyText.select();
-//   document.execCommand("copy"); 
-// }; 
