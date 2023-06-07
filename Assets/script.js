b63fb5d2e9df0cc1d9e83be145bbdc1a80170fe5
sarahbroton <sarah.broton@gmail.com>
2023-06-07 16:01:04 -0500

    randomcodepassword

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,22 @@
-// Assignment code here
-var passwordLength; 
-var chars =""; 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // generateBtn.onclick = popupInfo;
 
-// Write password to the #password input
-
 function popupInfo() {
   // while (true) {
-    passwordLength = parseInt(prompt("Choose between 8 and 128 characters in length."), 10);
+    var passwordLength = parseInt(prompt("Choose between 8 and 128 characters in length."), 10);
     if (passwordLength < 8 || passwordLength > 129) {
       alert("Invalid password length. Password needs to be greater than 8 and less than 129 characters.");
       popupInfo(); 
     }
-    // console.log("passwordLength", passwordLength);
-
-  //   else {
-  //     break
-  //   }
-  // }
   
-    var specialCharacters = confirm("Do you want special characters?");
-  // console.log("specialCharacter", specialCharacters)
+  var specialCharacters = confirm("Do you want special characters?");
 
   var numericCharacters = confirm("Do you want numeric characters?");
-  // console.log("numericCharacter", numericCharacters)
 
   var lowercaseCharacters = confirm("Do you want lowercase characters?");
-  // console.log("lowercaseCharacter", lowercaseCharacters)
 
   var uppercaseCharacters = confirm("Do you want uppercase characters?");
-  // console.log("uppercaseCharacter", uppercaseCharacters)
 
   var passwordChoices = {
     passwordLength: passwordLength,
@@ -43,14 +27,14 @@ function popupInfo() {
 
   };
 
-  // console.log(passwordChoices);
   return passwordChoices;   
 
 }
 
 
 function generatePassword(){
-  var options = popupInfo()
+  var options = popupInfo();
+  var chars ="";
   console.log(options.passwordLength); 
     if (options.specialCharacters){
       chars+="!@#$%^&%*()"
@@ -69,7 +53,7 @@ function generatePassword(){
 
     var passwordArray = []; 
     var characterBank = chars.split(""); 
-    console.log(characterBank.length); 
+    console.log(characterBank, chars); 
     // 
 
     for (let index = 0; index < options.passwordLength; index++) {
@@ -77,50 +61,9 @@ function generatePassword(){
       const element = characterBank[randomIndex];
       passwordArray.push(element); 
     }
-
-    console.log(passwordArray.join("")); 
-
-  var password = ""
-  // wrap in a for loop and run it as many times as passwordLength
-  // password += chars.charAt(Math.floor((Math.random()*chars.length)))
-
-  // for (var i = 0; i <=passwordLength; i++) {
-  //     var randomNumber = Math.floor(Math.random() * chars.passwordLength); 
-  //     password += chars.substring(randomNumber, randomNumber +1); 
-  
-  // for (generatePassword)
-  // split on the word ; "join"
-  // options.
-  // >129
-
-// for (index = 0; index < this.passwordLength; index++) {
-//   this.password +=
-//   this.generatePassword[Math.floor((math.random()* this.passwordLength)*specialCharacters*numericCharacters*lowercaseCharacters*uppercaseCharacters)]
-// }
-
-// document.getElementById("password").value=this.passwordChoices
-
-// var passwordChoices=document.getElementByID("password"); 
-
-// function generatePassword(){
-// var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// for (var i = 0; i <=passwordLength; i++) {
-//   var randomNumber = Math.floor(Math.random() * chars.passwordLength); 
-//   password += chars.substring(randomNumber, randomNumber +1); 
-// }
-// }
-// document.getElementById("password").value = password; 
-
-// function copyPassword() {
-//   var copyText = document.getElementById("password"); 
-//   copyText.select(); 
-//   document.execCommand("copy"); 
-
-
-// Add event listener to generate button
-  return chars;
+ 
+  return passwordArray.join(""); 
 }
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
